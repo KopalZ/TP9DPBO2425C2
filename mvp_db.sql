2a2834669a95c2d2d75c08d62aca5ab63cb2a0fd
KopalZ <naufalzahid.gamer@gmail.com>
2025-11-25 23:15:51 +0700

    Add files via upload

---
+++ mvp_db.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Nov 2025 pada 16.45
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mvp_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kendaraan`
+--
+
+CREATE TABLE `kendaraan` (
+  `id` int(11) NOT NULL,
+  `merk` varchar(100) NOT NULL,
+  `tipe` varchar(100) NOT NULL,
+  `cc_mesin` int(11) NOT NULL,
+  `top_speed` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kendaraan`
+--
+
+INSERT INTO `kendaraan` (`id`, `merk`, `tipe`, `cc_mesin`, `top_speed`) VALUES
+(1, 'Ducati', 'Desmosedici GP24', 1000, 360),
+(3, 'Honda', 'RC213V', 1000, 355),
+(4, 'Williams', 'FW46', 1600, 323),
+(6, 'McLaren', 'MCL38', 1600, 342);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembalap`
+--
+
+CREATE TABLE `pembalap` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tim` varchar(255) NOT NULL,
+  `negara` varchar(255) NOT NULL,
+  `poinMusim` int(11) DEFAULT 0,
+  `jumlahMenang` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pembalap`
+--
+
+INSERT INTO `pembalap` (`id`, `nama`, `tim`, `negara`, `poinMusim`, `jumlahMenang`) VALUES
+(1, 'Lewis Hamilton', 'Mercedes', 'United Kingdom', 347, 11),
+(2, 'Max Verstappen', 'Red Bull', 'Netherlands', 335, 10),
+(3, 'Valtteri Bottas', 'Mercedes', 'Finland', 203, 2),
+(4, 'Sergio Perez', 'Red Bull', 'Mexico', 190, 1),
+(5, 'Carlos Sainz', 'Ferrari', 'Spain', 150, 0),
+(6, 'Daniel Ricciardo', 'McLaren', 'Australia', 115, 1),
+(7, 'Charles Leclerc', 'Ferrari', 'Monaco', 95, 0),
+(8, 'Lando Norris', 'McLaren', 'United Kingdom', 88, 0),
+(9, 'Pierre Gasly', 'AlphaTauri', 'France', 75, 0),
+(10, 'Xabi Alonso', 'Madrid', 'Spain', 65, 2),
+(11, 'Kakek Mudrik', 'Manchester United', 'Malaysia', 70, 2),
+(12, 'Fransesco Bagnaia', 'Ducati Lenovo', 'Italy', 210, 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pembalap`
+--
+ALTER TABLE `pembalap`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembalap`
+--
+ALTER TABLE `pembalap`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
